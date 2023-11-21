e8257c0e3355fbab7d405eda4d45d143e0c59288
Kooper <74805059+Kooperlol@users.noreply.github.com>
2023-11-21 11:45:52 -0600

    first commit

---
+++ src/components/shared/bg-particles.tsx
@@ -0,0 +1,98 @@
+"use client";
+import React from "react";
+import { useCallback } from "react";
+import { loadSlim } from "tsparticles-slim";
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-particles";
+
+const BgParticles = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      console.log(container);
+    },
+    []
+  );
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#fefcfd",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#5f43b2",
+          },
+          links: {
+            color: "#5f43b2",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+};
+
+export default BgParticles;
---
+++ src/components/shared/header.tsx
@@ -0,0 +1,93 @@
+"use client";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+export default function Header() {
+  const [nav, setNav] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const updatePosition = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener("scroll", updatePosition);
+
+    updatePosition();
+
+    return () => window.removeEventListener("scroll", updatePosition);
+  }, []);
+
+  return (
+    <div className="absolute left-0 top-0 z-50 ease-in duration-300">
+      <div
+        className={`m-auto flex w-screen justify-between items-center px-8 py-5 text-white ${
+          scrollPosition > 0 ? "fixed drop-shadow-md" : "fixed shadow-none"
+        }`}
+      >
+        <Link href="/">
+          <Image
+            priority
+            draggable={false}
+            src="/media/logo.png"
+            alt="logo"
+            width={100}
+            height={100}
+          />
+        </Link>
+        <ul className="hidden text-black sm:flex">
+          <li className="p-4 hover:text-primary">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="p-4 hover:text-primary">
+            <Link href="/database">Database</Link>
+          </li>
+          <li className="p-4 hover:text-primary">
+            <Link href="/careers">Careers</Link>
+          </li>
+          <li className="p-4 hover:text-primary">
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        {/* Mobile Button */}
+        <div onClick={handleNav} className="block sm:hidden z-50">
+          {nav ? (
+            <AiOutlineClose size={20} />
+          ) : (
+            <AiOutlineMenu color="black" size={20} />
+          )}
+        </div>
+        {/* Mobile Menu */}
+        <div
+          className={
+            nav
+              ? "sm:hidden z-40 absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
+              : "sm:hidden z-40 absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
+          }
+        >
+          <ul>
+            <li onClick={handleNav} className="p-4 text-4xl hover:text-primary">
+              <Link href="/">Home</Link>
+            </li>
+            <li onClick={handleNav} className="p-4 text-4xl hover:text-primary">
+              <Link href="/database">Database</Link>
+            </li>
+            <li onClick={handleNav} className="p-4 text-4xl hover:text-primary">
+              <Link href="/careers">Careers</Link>
+            </li>
+            <li onClick={handleNav} className="p-4 text-4xl hover:text-primary">
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}

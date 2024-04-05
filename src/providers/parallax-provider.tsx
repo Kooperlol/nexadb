fa15dfa94c95a54315892c68e4d1091e951922c6
Kooper <74805059+Kooperlol@users.noreply.github.com>
2024-04-04 20:02:05 -0500

    Removed Parallax Provider

--- src/providers/parallax-provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { ParallaxProvider } from "react-scroll-parallax";
-
-export function ParallaxProvider_({ children }: { children: React.ReactNode }) {
-  return <ParallaxProvider>{children}</ParallaxProvider>;
-}
-
-export default ParallaxProvider_;
